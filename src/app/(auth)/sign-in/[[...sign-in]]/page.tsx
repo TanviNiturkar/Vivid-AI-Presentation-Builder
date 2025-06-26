96ffd9cdc38e0c1bf814fb3220f68035d7543341
Tanvi Niturkar <tanvipniturkar93@gmail.com>
2025-06-26 23:41:23 +0530

    sign-in

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,7 @@
 // app/(auth)/sign-in/page.tsx
 import { SignIn } from '@clerk/nextjs';
+import Link from 'next/link';
 
 export default function Signin() {
-  return <SignIn />;;
+  return <Link href="/sign-in?redirect_url=/dashboard">Sign In</Link>
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,7 @@
 // app/(auth)/sign-up/page.tsx
 import { SignUp } from '@clerk/nextjs';
+import Link from 'next/link';
 
 export default function Signup() {
-  return <SignUp />;
+  return <Link href="/sign-up?redirect_url=/dashboard">Sign Up</Link>
 }
