b29164b30048e8cbbdde45e8340f5cd60f6f31b7
Tanvi Niturkar <tanvipniturkar93@gmail.com>
2025-07-02 22:30:17 +0530

    Fix: Ensure /api/webhook/subscription is deployed

--- src/app/(protected)/(pages)/(dashboardpages)/dashboard/page.tsx
+++ src/app/(protected)/(pages)/(dashboardpages)/dashboard/page.tsx
@@ -2,12 +2,15 @@ import { getAllProjects } from "@/actions/project";
 import NotFound from "@/components/global/notfound";
 import Projects from "@/components/global/projects";
 import { onAuthenticateUser } from "@/actions/user"; // ✅
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
   try {
     const { user } = await onAuthenticateUser();
     const allProjects = await getAllProjects();
-
+    if(!user){
+      redirect('/');
+    }
     return (
       <div className="w-full flex flex-col gap-6 p-4">
         <div className="flex flex-col-reverse items-start w-full gap-6 sm:flex-row sm:justify-between sm:items-center">
